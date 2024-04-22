32409a8f433ee61c4e719a459673d5dd98350ca9
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-04-22 19:31:40 +0700

    fix: change start screen

--- app/src/main/java/com/example/h5traveloto_booking/navigate/AppNavigation.kt
+++ app/src/main/java/com/example/h5traveloto_booking/navigate/AppNavigation.kt
@@ -18,7 +18,7 @@ fun AppNavigation(startDestination : String ) {
 
     NavHost (
         navController = navController,
-        startDestination = Screens.LoginScreen.name
+        startDestination = Screens.MainScreen.name
 
     ) {
         composable(route = Screens.SignUpScreen.name) {
