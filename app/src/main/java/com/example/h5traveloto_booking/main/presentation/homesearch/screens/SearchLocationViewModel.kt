5e043aacefd1ef486479cee888092b7a938c7c23
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-05-10 20:26:07 +0700

    Update SearchLocationViewModel.kt
    Test Api

--- app/src/main/java/com/example/h5traveloto_booking/main/presentation/homesearch/screens/SearchLocationViewModel.kt
+++ app/src/main/java/com/example/h5traveloto_booking/main/presentation/homesearch/screens/SearchLocationViewModel.kt
@@ -60,7 +60,7 @@ class SearchLocationViewModel @Inject constructor(
             _districtsVN = response.districts
             Log.d("SearchLocationViewModel", "Districts VN: ${_districtsVN[0].name}")
         }
-        useCases.searchSuggestionUseCase(15,"Hà").onStart {
+        useCases.searchSuggestionUseCase(15,"gia lai").onStart {
 
         }
         .catch { e ->
